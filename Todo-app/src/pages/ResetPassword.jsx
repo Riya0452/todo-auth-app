e0e2ef0dc2a3bd54af13cc5beda42d7bf6b9a278
Riya Kumari <riyakumari@Riyas-MacBook-Air.local>
2026-04-16 23:06:05 +0530

    fixed css import

--- Todo-app/src/pages/ResetPassword.jsx
+++ Todo-app/src/pages/ResetPassword.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Auth.css";
+import "./auth.css";
 
 function ResetPassword() {
   const [id, setId] = useState("");
